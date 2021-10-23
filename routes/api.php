b698ff78cb7e5b63ea85868f9c7d9d1647acbf86
easymagic <nnamware@yahoo.com>
2021-10-24 00:00:21 +0100

    np

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Models\Profile;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,6 +19,16 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('migrate-fresh', function () {
+    // return Profile::all();
+    Artisan::call('migrate:fresh');
+    return [
+        'message'=>'Migrated.'
+    ];
+
+});
+
 
 Route::get('profiles', function () {
     return Profile::all();
